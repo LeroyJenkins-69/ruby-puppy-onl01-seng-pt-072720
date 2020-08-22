5cf9d886a9f0088fa67c77d0e2ae0cd2ed8fd816
Kordell Parrish <parrishkordell@gmail.com>
2020-08-22 18:44:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,12 @@
-class Dog
-  attr_accessor :name
 
+class Dog
   @@all = []
+
+  name:attr_accessor
 
   def initialize(name)
     @name = name
-    save
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.print_all
-    puts @@all.map{ |dog| dog.name }
-  end
-
-  def self.clear_all
-    @@all.clear
-  end
-
-  def save
     @@all << self
   end
+  
 end
